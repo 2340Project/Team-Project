57c6e0e9ced35fa9c73908de52d0d890fe18e015
Yamin <ymousselli@gmail.com>
2017-04-01 22:09:26 -0400

    Still Working

--- AllStarWaterCrowdSourcingApp/app/src/main/java/com/allstarproject/cs2340/allstarwatercrowdsourcingapp/controller/RegisterActivity.java
+++ AllStarWaterCrowdSourcingApp/app/src/main/java/com/allstarproject/cs2340/allstarwatercrowdsourcingapp/controller/RegisterActivity.java
@@ -15,7 +15,8 @@ import com.allstarproject.cs2340.allstarwatercrowdsourcingapp.model.RegisteredUs
 
 import java.io.File;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+public class RegisterActivity extends AppCompatActivity
+        implements View.OnClickListener {
     private Spinner spinner;
     private EditText txtUserName;
     private EditText txtName;
@@ -24,9 +25,12 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private EditText txtConfPassword;
 
     /**
-     * onCreate used to setup RegisterActivity on creation
-     *
-     * @param savedInstanceState is bundled data passed in to use at creation
+     * onCreate method to instantiate the necessary buttons and text fields
+     * for the Registration Screen.
+     * @param savedInstanceState the data which Android saves to populate
+     * data more quickly than the application starting up. It's basically
+     * caching everything so load up time is quicker when going back to the
+     * screen.
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,59 +62,82 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()) {
 
-            case R.id.btnCancelRegister:
-                Intent intent = new Intent(RegisterActivity.this, WelcomeActivity.class);
-                startActivity(intent);
-                break;
+        case R.id.btnCancelRegister:
+            Intent intent = new Intent(RegisterActivity.this,
+                    WelcomeActivity.class);
+            startActivity(intent);
+            break;
 
-            case R.id.btnSubmitRegister:
-                Intent intent2 = new Intent(RegisterActivity.this, LoginActivity.class);
-                if (txtConfPassword.getText().toString().isEmpty() || txtPassword.getText().toString().isEmpty()
-                        || txtUserName.getText().toString().isEmpty() || txtPassword.getText().toString().isEmpty()
-                        || txtName.getText().toString().isEmpty() || txtEmail.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplication(), "Missing data entry", Toast.LENGTH_LONG).show();
-                } else if (txtConfPassword.getText().toString().equals(txtPassword.getText().toString())) {
-                    if (spinner.getSelectedItem().toString().equals("User")) {
-                        new RegisteredUser(txtUserName.getText().toString()
-                                , txtPassword.getText().toString(), txtName.getText().toString()
-                                , txtEmail.getText().toString(), spinner.getSelectedItem().toString());
-                        ModelFacade modelFacade = ModelFacade.getModelFacade();
-                        File file = new File(this.getFilesDir(), ModelFacade.DEFAULT_BINARY_FILE_NAME);
-                        Log.d("Saving", "About to save data. . .");
-                        modelFacade.saveBinary(file);
-                        startActivity(intent2);
-                    } else if (spinner.getSelectedItem().toString().equals("Manager")) {
-                        new RegisteredUser(txtUserName.getText().toString()
-                                , txtPassword.getText().toString(), txtName.getText().toString()
-                                , txtEmail.getText().toString(), spinner.getSelectedItem().toString());
-                        ModelFacade modelFacade = ModelFacade.getModelFacade();
-                        File file = new File(this.getFilesDir(), ModelFacade.DEFAULT_BINARY_FILE_NAME);
-                        Log.d("Saving", "About to save data. . .");
-                        modelFacade.saveBinary(file);
-                        startActivity(intent2);
-                    } else if (spinner.getSelectedItem().toString().equals("Worker")) {
-                        new RegisteredUser(txtUserName.getText().toString()
-                                , txtPassword.getText().toString(), txtName.getText().toString()
-                                , txtEmail.getText().toString(), spinner.getSelectedItem().toString());
-                        ModelFacade modelFacade = ModelFacade.getModelFacade();
-                        File file = new File(this.getFilesDir(), ModelFacade.DEFAULT_BINARY_FILE_NAME);
-                        Log.d("Saving", "About to save data. . .");
-                        modelFacade.saveBinary(file);
-                        startActivity(intent2);
-                    } else if (spinner.getSelectedItem().toString().equals("Admin")) {
-                        new RegisteredUser(txtUserName.getText().toString()
-                                , txtPassword.getText().toString(), txtName.getText().toString()
-                                , txtEmail.getText().toString(), spinner.getSelectedItem().toString());
-                        ModelFacade modelFacade = ModelFacade.getModelFacade();
-                        File file = new File(this.getFilesDir(), ModelFacade.DEFAULT_BINARY_FILE_NAME);
-                        Log.d("Saving", "About to save data. . .");
-                        modelFacade.saveBinary(file);
-                        startActivity(intent2);
-                    } else {
-                        Toast.makeText(getApplication(), "User type not selected", Toast.LENGTH_LONG).show();
-                    }//this is here for checkstyle.
+        case R.id.btnSubmitRegister:
+            Intent intent2 = new Intent(RegisterActivity.this,
+                    LoginActivity.class);
+            if (txtConfPassword.getText().toString().isEmpty()
+                    || txtPassword.getText().toString().isEmpty()
+                    || txtUserName.getText().toString().isEmpty()
+                    || txtPassword.getText().toString().isEmpty()
+                    || txtName.getText().toString().isEmpty()
+                    || txtEmail.getText().toString().isEmpty()) {
+                Toast.makeText(getApplication(), "Missing data entry",
+                        Toast.LENGTH_LONG).show();
+            } else if (txtConfPassword.getText().toString().equals(
+                    txtPassword.getText().toString())) {
+                if (spinner.getSelectedItem().toString().equals("User")) {
+                    new RegisteredUser(txtUserName.getText().toString(),
+                            txtPassword.getText().toString(),
+                            txtName.getText().toString(),
+                            txtEmail.getText().toString(),
+                            spinner.getSelectedItem().toString());
+                    ModelFacade modelFacade = ModelFacade.getModelFacade();
+                    File file = new File(this.getFilesDir(),
+                            ModelFacade.DEFAULT_BINARY_FILE_NAME);
+                    Log.d("Saving", "About to save data. . .");
+                    modelFacade.saveBinary(file);
+                    startActivity(intent2);
+                } else if (spinner.getSelectedItem().toString().equals(
+                        "Manager")) {
+                    new RegisteredUser(txtUserName.getText().toString(),
+                            txtPassword.getText().toString(),
+                            txtName.getText().toString(),
+                            txtEmail.getText().toString(),
+                            spinner.getSelectedItem().toString());
+                    ModelFacade modelFacade = ModelFacade.getModelFacade();
+                    File file = new File(this.getFilesDir(),
+                            ModelFacade.DEFAULT_BINARY_FILE_NAME);
+                    Log.d("Saving", "About to save data. . .");
+                    modelFacade.saveBinary(file);
+                    startActivity(intent2);
+                } else if (spinner.getSelectedItem().toString().equals(
+                        "Worker")) {
+                    new RegisteredUser(txtUserName.getText().toString(),
+                            txtPassword.getText().toString(),
+                            txtName.getText().toString(),
+                            txtEmail.getText().toString(),
+                            spinner.getSelectedItem().toString());
+                    ModelFacade modelFacade = ModelFacade.getModelFacade();
+                    File file = new File(this.getFilesDir(),
+                            ModelFacade.DEFAULT_BINARY_FILE_NAME);
+                    Log.d("Saving", "About to save data. . .");
+                    modelFacade.saveBinary(file);
+                    startActivity(intent2);
+                } else if (spinner.getSelectedItem().toString().equals(
+                        "Admin")) {
+                    new RegisteredUser(txtUserName.getText().toString(),
+                            txtPassword.getText().toString(),
+                            txtName.getText().toString(),
+                            txtEmail.getText().toString(),
+                            spinner.getSelectedItem().toString());
+                    ModelFacade modelFacade = ModelFacade.getModelFacade();
+                    File file = new File(this.getFilesDir(),
+                            ModelFacade.DEFAULT_BINARY_FILE_NAME);
+                    Log.d("Saving", "About to save data. . .");
+                    modelFacade.saveBinary(file);
+                    startActivity(intent2);
+                } else {
+                    Toast.makeText(getApplication(), "User type not selected",
+                            Toast.LENGTH_LONG).show();
                 }
-                default:
+            }
+        default:
         }
     }
 }
