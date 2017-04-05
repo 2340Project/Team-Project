958c6a47e366d71636cfc3fcb30df404a92021c6
Yamin <ymousselli@gmail.com>
2017-04-05 17:22:39 -0400

    Cleaned up JUNIT

--- AllStarWaterCrowdSourcingApp/app/src/test/java/com/allstarproject/cs2340/allstarwatercrowdsourcingapp/PrintPurityReportsTest.java
+++ AllStarWaterCrowdSourcingApp/app/src/test/java/com/allstarproject/cs2340/allstarwatercrowdsourcingapp/PrintPurityReportsTest.java
@@ -7,47 +7,40 @@ import com.allstarproject.cs2340.allstarwatercrowdsourcingapp.model.ModelFacade;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 
 public class PrintPurityReportsTest {
     private String list = "";
     private ModelFacade modelFacade = ModelFacade.getModelFacade();
     private Model model = modelFacade.getModelInstance();
-    //private final java.util.Date date;
-    //private final String dateAndTime;
-
-
 
     @Before
     public void setUp() {
         model.setUser();
-        model.addPurityReportList("Atlanta", 2.256, "Bio-Hazards", "Contaminant");
-        model.addPurityReportList("Washington D.C.", 3.123, "Erosion", "Virus");
-        model.addPurityReportList("Tampa Bay", 4.0123, "Pesticides", "Contaminant");
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
-                Locale.US);
-        //date = new java.util.Date();
-        //dateAndTime = dateFormat.format(date);
+        model.addPurityReportList("Atlanta", 2.256, "Bio-Hazards",
+                "Contaminant");
+        model.addPurityReportList("Washington D.C.", 3.123, "Erosion",
+                "Virus");
+        model.addPurityReportList("Tampa Bay", 4.0123, "Pesticides",
+                "Contaminant");
 
 
+        list += ("1. " + model.getLastDateAndTime() + " , "
+                + "Submitted by: " + "" + "austin"  + " , " + "Atlanta"
+                + ": " + 2.256 + " Virus/Contaminant PPM , "
+                + "Bio-Hazards" + "\n");
 
-        list += (1 + ". "   + " , "
-                + "Submitted by: " + "" + "aduncan37"  + " , " + "Atlanta" + ": "
-                + 2.256 + " Virus/Contaminant PPM , "  + "Bio-Hazards" + "\n");
-
-        list += (2 + ". "  + " , "
-                + "Submitted by: " + "" + "aduncan37"  + " , " + "Washington D.C." + ": "
-                + 3.123 + " Virus/Contaminant PPM , "  + "Erosion" + "\n");
+        list += ("2. " + model.getLastDateAndTime() + " , "
+                + "Submitted by: " + "" + "austin"  + " , "
+                + "Washington D.C." + ": " + 3.123 + " Virus/Contaminant PPM , "
+                + "Erosion" + "\n");
 
 
-        list += (3 + ". "  + " , "
-                + "Submitted by: " + "" + "aduncan37"  + " , " + "Tampa Bay" + ": "
-                + 4.0123 + " Virus/Contaminant PPM , "  + "Pesticides" + "\n");
+        list += ("3. " + model.getLastDateAndTime() +  " , "
+                + "Submitted by: " + "" + "austin"  + " , " + "Tampa Bay"
+                + ": " + 4.0123 + " Virus/Contaminant PPM , "
+                + "Pesticides" + "\n");
 
-}
+    }
     @Test
     public void hasAllPurityReports() {
         assertEquals(list, model.printPurityReports());
